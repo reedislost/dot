eb5790dfd647363a201023ecf3459dfee1a4e4e9
reedislost <93827760+reedislost@users.noreply.github.com>
2022-11-11 08:44:48 -0700

    Delete dot.sh

--- dot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Ask the user for DOT Number
-echo Enter DOT Number
-read dot
-# fetch data and put it into a json file
-curl https://mobile.fmcsa.dot.gov/qc/services/carriers/"$dot"?webKey=ebef3f068cb2c38b7a07de20385ff03e977964df >> carriers/$dot.json
-
-# open json file in jless
-jless carriers/$dot.json
